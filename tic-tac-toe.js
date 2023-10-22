d1c6bfdbbcc370b577cf41892604c5b5e9897092
McKoy-Banton <mckoybanton1@gmail.com>
2023-10-22 18:48:34 -0500

    Ex1

---
+++ tic-tac-toe.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const board = document.getElementById("board")
+    const squares = Array.from(board.getElementsByTagName("div"))
+    const newGameButton = document.querySelector(".btn")
+    const status = document.getElementById("status");
+})
